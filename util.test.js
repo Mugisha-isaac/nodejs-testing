1d2b5d4073901d57794b928c279f0d1612197fc0
Mugisha-isaac <mugishaisaac2001@gmail>
2022-05-25 12:22:56 +0200

    test(util.test.js) unit tests

--- util.test.js
+++ util.test.js
@@ -2,5 +2,11 @@ const {generateText} = require('./util');
 
 test('should output name and age',()=>{
     const text = generateText('Max',29);
-    expect(text).toBe('Max (29 years old)');
+    expect(text).toBe('Max (29 years old)'); 
 });
+
+
+test('should output data-less text',()=>{
+    const text = generateText();
+    expect(text).toBe('undefined (undefined years old)');  
+})
